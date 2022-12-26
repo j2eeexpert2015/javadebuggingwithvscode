c60b71bd8bfb19755a9d4766973ae23b278f9ea6
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-12-26 10:30:50 +0530

    Added new files

---
+++ src/main/java/org/debugtutorial/problemsolving/solutions/simple/StringUtilitySolution.java
@@ -0,0 +1,67 @@
+package org.debugtutorial.problemsolving.solutions.simple;
+import java.util.Scanner;
+
+/**
+ * This program implements two methods related to String which has issues
+ */
+public class StringUtilitySolution {
+    /**
+     * Main program to test the methods countOccurrences and reverseString
+     *
+     */
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = stdin.next();
+
+        System.out.println("Enter a char: ");
+        char ch = stdin.next().charAt(0);
+
+        System.out.println("Character " + ch + " occurs "
+                + countOfccurrences(str, ch) + " times in string \""
+                + str + "\".");
+        System.out.println("\n\nEnter another String: ");
+        str = stdin.next();
+
+        System.out.println("The reverse of string \""
+                + str + "\" is " + reverseString(str));
+
+    }
+
+    /**
+     * Counts the number of occurrences of char ch in String str.
+     */
+    public static int countOfccurrences(String inputStr, char c) {
+        int count = 0;
+        if (inputStr != null) {
+            for (int i = 0; i < inputStr.length(); i++)
+            {
+                if (inputStr.charAt(i) == c) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Reverses the String.
+     */
+    public static String reverseString(String inputStr) {
+        //toCharArray converts string s to an array of characters
+        char[] charArray = inputStr.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            char cTmp = charArray[i];
+            charArray[i] = charArray[charArray.length-i-1];
+            charArray[charArray.length-i-1] = cTmp;
+            if(i >=(charArray.length-i-1))
+            {
+                break;
+            }
+        }
+
+        //converts the char array of characters to a String
+        return new String(charArray);
+    }
+}

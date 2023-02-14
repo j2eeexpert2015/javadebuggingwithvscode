3e687274d18a6c491b031492e96cec6605dc7def
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2023-02-14 21:58:17 +0530

    Adding JUnit Samples

---
+++ src/test/java/org/debugtutorial/util/CalculatorUtilTest.java
@@ -0,0 +1,28 @@
+package org.debugtutorial.util;
+
+//import static org.junit.Assert.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorUtilTest {
+
+	@Test
+	@DisplayName("1 + 1 = 2")
+	void addsTwoNumbers() {
+		CalculatorUtil calculatorUtil = new CalculatorUtil();
+		assertEquals(2, calculatorUtil.add(1, 1), "1 + 1 should equal 2");
+	}
+
+	@Disabled
+	@Test
+	@DisplayName("Validate Division By Zero ")
+	void validateDivision() {
+		CalculatorUtil calculatorUtil = new CalculatorUtil();
+		assertEquals(2, calculatorUtil.divide(10, 0), "Validate 10/0 ");
+	}
+}
